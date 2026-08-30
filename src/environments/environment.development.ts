a640de065360bfa4b438bb909d68bbd9fe5e0ddb
nerdwerd <nerdwerd@gangoons.com>
2026-08-30 16:03:55 +0000

    Delete environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  // Your central configuration hub address variable
-  apiUrl: 'https://localhost:7173/api',
-  patientPortalUrl: 'http://localhost:4200',
-  practicePortalUrl: 'http://localhost:4300',
-  mapBoxAPI: 'https://api.mapbox.com/search/geocode/v6/forward?q=',
-  mapBoxToken: 'pk.eyJ1IjoiYXplcmFpbHQiLCJhIjoiY21zNHNjcGF3MGZ5dzJ4cjR3amxqejF5NyJ9.fG-6d9hZ_lo4P0r5HB4o_A',
-};
