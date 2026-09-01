1116e82775b2833abf952d52e4bbbac489c36e6a
nerdwerd <nerdwerd@gangoons.com>
2026-09-01 17:18:18 +0200

    Added tests to appointment service

--- src/app/services/appointment.service.spec.ts
+++ src/app/services/appointment.service.spec.ts
@@ -1,16 +1,66 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppointmentService } from './appointment.service';
+import {HttpTestingController, provideHttpClientTesting} from '@angular/common/http/testing';
+import {environment} from '../../environments/environment.development';
+import {Appointments} from '@shared/models/appointment.types';
 
 describe('AppointmentService', () => {
   let service: AppointmentService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AppointmentService, provideHttpClientTesting()]
+    });
     service = TestBed.inject(AppointmentService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  afterEach(() =>{
+    httpMock.verify();
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a list of appointments', () =>{
+    const dummyUserId = 'user123';
+    const mockAppointments: Appointments[] = [
+      { id: '1', patientId: 'pat-12', doctorId: 'doc-123', scheduleTime: new Date() },
+      { id: '2', patientId: 'pat-34', doctorId: 'doc-456', scheduleTime: new Date() },
+    ];
+
+    service.getAppointmentsFor(dummyUserId).subscribe((appointments) => {
+      expect(appointments.length).toBe(2);
+      expect(appointments).toEqual(mockAppointments);
+    });
+
+    const expectedUrl = `${environment.apiUrl}/get/user/${dummyUserId}/appointments`;
+    const req = httpMock.expectOne(expectedUrl);
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockAppointments);
+  })
+
+  it('should create a new appointment', () =>{
+    const mockAppointment: Appointments = {
+      id: '1', patientId: 'pat-12', doctorId: 'doc-123',
+        scheduleTime: new Date() };
+
+    service.createAppointment(mockAppointment).subscribe((appointment) => {
+      expect(appointment).toEqual(mockAppointment);
+    });
+
+    const expectedUrl = `${environment.apiUrl}/create/appointment`;
+    const req = httpMock.expectOne(expectedUrl);
+
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(mockAppointment);
+
+    req.flush(mockAppointment);
+  })
+
 });
