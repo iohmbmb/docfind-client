2c3e3c40f4d7bef82b2358fa2fc1e1ce9dcc5978
nerdwerd <nerdwerd@gangoons.com>
2026-09-08 21:02:59 +0200

    modified general preference for practice

--- projects/practice/src/app/components/preferences-general/preferences-general.spec.ts
+++ projects/practice/src/app/components/preferences-general/preferences-general.spec.ts
@@ -1,12 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PreferencesGeneral } from './preferences-general';
+
+declare global {
+  interface Window {
+    feather: {
+      replace: () => void;
+    } | undefined;
+  }
+}
 
 describe('PreferencesGeneral', () => {
   let component: PreferencesGeneral;
   let fixture: ComponentFixture<PreferencesGeneral>;
 
   beforeEach(async () => {
+    window.feather = {
+      replace: () =>{}
+    }
+
     await TestBed.configureTestingModule({
       imports: [PreferencesGeneral],
     }).compileComponents();
@@ -15,6 +26,10 @@ describe('PreferencesGeneral', () => {
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
+
+  afterEach(() => {
+    window.feather = undefined;
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
