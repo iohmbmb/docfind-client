f6388c4030cda0fd05e0b6f364e1a84b314ccfd4
nerdwerd <nerdwerd@gangoons.com>
2026-09-08 21:01:27 +0200

    added tests for signup

--- src/app/components/signup/signup.spec.ts
+++ src/app/components/signup/signup.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SignupComponent } from './signup';
-import {provideRouter} from '@angular/router';
+import {provideRouter, Router} from '@angular/router';
+import { UserRole } from "@shared/models/user.types";
+import {HttpTestingController, provideHttpClientTesting} from "@angular/common/http/testing";
+import { environment } from "src/environments/environment.development";
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -10,7 +12,7 @@ describe('SignupComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SignupComponent],
-      providers: [provideRouter([])]
+      providers: [provideRouter([]), provideHttpClientTesting()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignupComponent);
@@ -21,4 +23,155 @@ describe('SignupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display email error message', () => {
+    const emailInput = fixture.nativeElement.querySelector('[data-testid="email-field"]');
+    let emailError = fixture.nativeElement.querySelector('[data-testid="email-error"]');
+    expect(emailError).toBeNull();
+    emailInput.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+    emailError = fixture.nativeElement.querySelector('[data-testid="email-error"]');
+    expect(emailError).toBeTruthy();
+    expect(emailError.textContent).toContain('Email is required');
+  });
+
+  it('should display password error messages', () =>{
+    const passwordInput = fixture.nativeElement.querySelector('[data-testid="password-field"]');
+    let passwordError = fixture.nativeElement.querySelector('[data-testid="password-error"]');
+    passwordInput.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+    passwordError = fixture.nativeElement.querySelector('[data-testid="password-error"]');
+    expect(passwordError).toBeTruthy();
+    expect(passwordError.textContent).toContain('Password is required');
+  });
+
+  it('should update password rules indicator', () =>{
+    const passwordInput = fixture.nativeElement.querySelector('[data-testid="password-field"]');
+    let passrule1 = fixture.nativeElement.querySelector('[data-testid="pass-rule-1"]');
+    let passrule2 = fixture.nativeElement.querySelector('[data-testid="pass-rule-2"]');
+    let passrule3 = fixture.nativeElement.querySelector('[data-testid="pass-rule-3"]');
+    let passrule4 = fixture.nativeElement.querySelector('[data-testid="pass-rule-4"]');
+    let passrule5 = fixture.nativeElement.querySelector('[data-testid="pass-rule-5"]');
+    expect(passrule1.textContent).toEqual('•');
+    expect(passrule2.textContent).toEqual('•');
+    expect(passrule3.textContent).toEqual('•');
+    expect(passrule4.textContent).toEqual('•');
+    expect(passrule5.textContent).toEqual('•');
+
+    passwordInput.value = 'Po87@ejU';
+    passwordInput.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    passrule1 = fixture.nativeElement.querySelector('[data-testid="pass-rule-1"]');
+    passrule2 = fixture.nativeElement.querySelector('[data-testid="pass-rule-2"]');
+    passrule3 = fixture.nativeElement.querySelector('[data-testid="pass-rule-3"]');
+    passrule4 = fixture.nativeElement.querySelector('[data-testid="pass-rule-4"]');
+    passrule5 = fixture.nativeElement.querySelector('[data-testid="pass-rule-5"]');
+    expect(passrule1.textContent).toEqual('✓');
+    expect(passrule2.textContent).toEqual('✓');
+    expect(passrule3.textContent).toEqual('✓');
+    expect(passrule4.textContent).toEqual('✓');
+    expect(passrule5.textContent).toEqual('✓');
+  });
+
+  it('should display firstName error message', () => {
+    const firstnameInput = fixture.nativeElement.querySelector('[data-testid="firstname-field"]');
+    let firstnameError = fixture.nativeElement.querySelector('[data-testid="firstname-error"]');
+    firstnameInput.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+    firstnameError = fixture.nativeElement.querySelector('[data-testid="firstname-error"]');
+    expect(firstnameError).toBeTruthy();
+    expect(firstnameError.textContent).toEqual('First name is required');
+  });
+
+  it('should display lastName error message', () => {
+    const lastnameInput = fixture.nativeElement.querySelector('[data-testid="lastname-field"]');
+    let lastnameError = fixture.nativeElement.querySelector('[data-testid="lastname-error"]');
+    lastnameInput.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+    lastnameError = fixture.nativeElement.querySelector('[data-testid="lastname-error"]');
+    expect(lastnameError).toBeTruthy();
+    expect(lastnameError.textContent).toEqual('Last name is required');
+  });
+
+  it('should update submit button', () => {
+    let submitBtn = fixture.nativeElement.querySelector('[data-testid="btn-submit"]');
+    expect(submitBtn.textContent).toEqual(' Create account');
+    submitBtn.click();
+    component.isLoading.set(true);
+    fixture.detectChanges();
+    submitBtn = fixture.nativeElement.querySelector('[data-testid="btn-submit"]');
+    expect(submitBtn.textContent).toEqual(' Registering...');
+  })
+
+  it('should submit the form', async () => {
+    const router = TestBed.inject(Router);
+    const httpTestingController = TestBed.inject(HttpTestingController);
+    const spyOnRouter = vi.spyOn(router, 'navigate').mockResolvedValue(true);
+    const spyOnSubmit = vi.spyOn(component, 'onSubmit');
+    localStorage.clear();
+
+    component.signupModel.set({
+      email: 'test@mail.com',
+      password: 'Po87@ejU',
+      firstName: 'valid-firstname',
+      lastName: 'valid-lastname',
+      role: UserRole.patient
+    });
+
+    component.rememberMe = true;
+    fixture.detectChanges();
+
+    component.onSubmit();
+    fixture.detectChanges();
+
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/auth/register`);
+    expect(req.request.method).toBe('POST');
+    req.flush({});
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+
+    expect(spyOnSubmit).toHaveBeenCalled();
+    expect(localStorage.length).toEqual(1);
+    expect(component.isLoading()).toEqual(false);
+    expect(spyOnRouter).toHaveBeenCalledWith(['/bookings']);
+  })
+
+  it('should submit the form and catch an error', async () => {
+    const router = TestBed.inject(Router);
+    const httpTestingController = TestBed.inject(HttpTestingController);
+    const spyOnRouter = vi.spyOn(router, 'navigate').mockResolvedValue(true);
+    const spyOnSubmit = vi.spyOn(component, 'onSubmit');
+    localStorage.clear();
+
+    component.signupModel.set({
+      email: 'test@mail.com',
+      password: 'Po87@ejU',
+      firstName: 'valid-firstname',
+      lastName: 'valid-lastname',
+      role: UserRole.patient
+    });
+
+    component.rememberMe = false;
+    fixture.detectChanges();
+
+    component.onSubmit();
+    fixture.detectChanges();
+
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/auth/register`);
+    expect(req.request.method).toBe('POST');
+    req.error(new ProgressEvent('error'), {
+      status: 409,
+      statusText: 'Conflict'
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+
+    expect(spyOnSubmit).toHaveBeenCalled();
+    expect(localStorage.length).toEqual(0);
+    expect(component.isLoading()).toEqual(false);
+    expect(component.errorMessage()).toEqual('User already exists or registration rejected.');
+    expect(spyOnRouter).not.toHaveBeenCalled();
+  })
 });
