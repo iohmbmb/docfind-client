2dcb9d74c2f165ff7a96620f00fdc3593a7e1110
nerdwerd <nerdwerd@gangoons.com>
2026-09-08 21:03:32 +0200

    modified tests for update password component for practicer

--- projects/practice/src/app/components/update-password/update-password.spec.ts
+++ projects/practice/src/app/components/update-password/update-password.spec.ts
@@ -1,20 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {MatDialogRef} from '@angular/material/dialog';
 import { UpdatePassword } from './update-password';
+import {provideRouter} from '@angular/router';
+
+declare global {
+  interface Window {
+    feather: {
+      replace: () => void;
+    } | undefined;
+  }
+}
 
 describe('UpdatePassword', () => {
   let component: UpdatePassword;
   let fixture: ComponentFixture<UpdatePassword>;
 
   beforeEach(async () => {
+    window.feather = {
+      replace: () =>{}
+    }
+
     await TestBed.configureTestingModule({
       imports: [UpdatePassword],
+      providers: [provideRouter([]), {provide: MatDialogRef, useValue: {}}]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UpdatePassword);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
+
+  afterEach(() => {
+    window.feather = undefined;
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
