63c980fed08903e2bdf0f50aa1d2f9cb7de24f16
nerdwerd <nerdwerd@gangoons.com>
2026-09-08 21:02:49 +0200

    modified preference tests for practice

--- projects/practice/src/app/components/preferences/preferences.spec.ts
+++ projects/practice/src/app/components/preferences/preferences.spec.ts
@@ -1,12 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Preferences } from './preferences';
+
+declare global {
+  interface Window {
+    feather: {
+      replace: () => void;
+    } | undefined;
+  }
+}
 
 describe('Preferences', () => {
   let component: Preferences;
   let fixture: ComponentFixture<Preferences>;
 
   beforeEach(async () => {
+    window.feather = {
+      replace: () =>{}
+    }
+
     await TestBed.configureTestingModule({
       imports: [Preferences],
     }).compileComponents();
@@ -15,6 +26,10 @@ describe('Preferences', () => {
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
+
+  afterEach(() => {
+    window.feather = undefined;
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
