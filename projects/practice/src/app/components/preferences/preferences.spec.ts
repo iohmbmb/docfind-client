670fbef79b8e25a17821b85837dde18c046e510f
nerdwerd <nerdwerd@gangoons.com>
2026-09-09 15:19:50 +0200

    aq

--- projects/practice/src/app/components/preferences/preferences.spec.ts
+++ projects/practice/src/app/components/preferences/preferences.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Preferences } from './preferences';
+import {HttpTestingController, provideHttpClientTesting} from '@angular/common/http/testing';
 
 declare global {
   interface Window {
@@ -12,6 +13,7 @@ declare global {
 describe('Preferences', () => {
   let component: Preferences;
   let fixture: ComponentFixture<Preferences>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     window.feather = {
@@ -20,8 +22,10 @@ describe('Preferences', () => {
 
     await TestBed.configureTestingModule({
       imports: [Preferences],
+      providers: [provideHttpClientTesting() ],
     }).compileComponents();
 
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(Preferences);
     component = fixture.componentInstance;
     await fixture.whenStable();
@@ -29,7 +33,12 @@ describe('Preferences', () => {
 
   afterEach( async () => {
     window.feather = undefined;
-    await new Promise(resolve => setTimeout(resolve, 20));
+    try {
+      const pendingRequests = httpTestingController.match(() => true);
+      pendingRequests.forEach(req => req.flush({}));
+    } catch (e) {
+    }
+    await new Promise(resolve => setTimeout(resolve, 10));
     if (fixture) {
       fixture.destroy();
     }
