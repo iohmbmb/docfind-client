fc8705d29cd1ac72f9056a88511da4bb152655e6
nerdwerd <nerdwerd@gangoons.com>
2026-09-13 11:21:05 +0200

    fixed update of current path for navbar for practice

--- projects/practice/src/app/components/navbar/navbar.ts
+++ projects/practice/src/app/components/navbar/navbar.ts
@@ -1,7 +1,9 @@
 import {AuthService} from '@shared/services/auth.service';
 import {environment} from '../../../../../../src/environments/environment.development';
 import {Component, inject, computed, signal} from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
+import {NavigationEnd, Router, RouterLink} from '@angular/router';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -15,7 +17,7 @@ export class Navbar {
   public authService = inject(AuthService);
   public patientPortalUrl = environment.patientPortalUrl;
 
-  public currentPath = signal<string>('');
+  public currentPath= signal<string>('');
 
   public isLoginPage = computed(() => this.currentPath() === '/login');
   public isSignupPage = computed(() => this.currentPath() === '/signup');
@@ -26,7 +28,10 @@ export class Navbar {
   }
 
   constructor(private router: Router) {
-    this.currentPath.set(this.router.url);
+    router.events.pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+    takeUntilDestroyed()).subscribe(() => {
+      this.currentPath.set(router.url);
+    })
   }
 
   public onSettings(){
