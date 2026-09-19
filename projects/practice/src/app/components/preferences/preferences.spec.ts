b6f4284a4dd6dc6e211d24bca81a097befe8c507
nerdwerd <nerdwerd@gangoons.com>
2026-09-19 17:48:46 +0200

    try fix of preferences test\

--- projects/practice/src/app/components/preferences/preferences.spec.ts
+++ projects/practice/src/app/components/preferences/preferences.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Preferences } from './preferences';
+import { PreferencesGeneral } from '../preferences-general/preferences-general';
+import {Component} from '@angular/core';
 
 declare global {
   interface Window {
@@ -9,16 +11,25 @@ declare global {
   }
 }
 
+@Component({
+  selector: 'app-preferences-general',
+  template: '',
+  standalone: true
+})
+class MockPreferencesGeneralComponent { }
+
 describe('Preferences', () => {
   let component: Preferences;
   let fixture: ComponentFixture<Preferences>;
-
   beforeEach(async () => {
     window.feather = {
     replace: () => {}
     }
     await TestBed.configureTestingModule({
       imports: [Preferences],
+    }).overrideComponent(Preferences, {
+      remove: { imports: [PreferencesGeneral] },
+      add: { imports: [MockPreferencesGeneralComponent] }
     }).compileComponents();
 
     fixture = TestBed.createComponent(Preferences);
